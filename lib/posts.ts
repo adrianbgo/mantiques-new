44962d73aa03d8091a4793017d1aecf84232bb5d
adrianbgo <adrianbgo@hotmail.com>
2023-04-24 08:50:42 -0400

    test: gh-pages

--- lib/posts.ts
+++ lib/posts.ts
@@ -23,7 +23,11 @@ export const getPostData = async (id: ObjectId) => {
   let dev = process.env.NODE_ENV !== "production";
   let { DEV_URL, PROD_URL } = process.env;
   const response: Response = await fetch(
-    `${dev ? DEV_URL : PROD_URL}/api/posts`
+    `${
+      dev
+        ? "http://localhost:3000"
+        : "https://adrianbgo.github.io/mantiques-new"
+    }/api/posts`
   );
   let data = await response.json();
   let posts: Array<BlogPost> = JSON.parse(JSON.stringify(data.message));
